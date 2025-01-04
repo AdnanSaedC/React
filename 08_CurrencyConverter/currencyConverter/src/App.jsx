cf7c873a706024fc3db7df6e881fd1787fd0a485
adnan <adnansaed180@gmail.com>
2025-01-05 00:09:57 +0530

    currencyConverterInitialStuff

---
+++ 08_CurrencyConverter/currencyConverter/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './index.css'
+
+function App(){
+return(
+  <>
+  <div className="bg-black w-full h-screen text-white flex justify-center">
+    <div className="place-content-center justify-content bg-gray-500">
+      <h1>Adnan</h1>
+    </div>
+  </div>
+  </>
+)
+}
+
+export default App;
